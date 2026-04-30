50ea105ab759f4876ee3ad653940d30d4ca596fb
Mario <marioalamolcoz@email.com>
2026-04-30 13:15:52 +0200

    Cambios maria

--- Gestion/gestionAlumno.vb
+++ Gestion/gestionAlumno.vb
@@ -51,7 +51,6 @@ Public Class GestionAlumno
                 End If
             End If
         End If
-
         Try
             conexion.Open()
             Dim sql As String = "SELECT DNI FROM ALUMNOS WHERE DNI=@DNI"
@@ -157,7 +156,7 @@ Public Class GestionAlumno
             Return False
 
         Catch ex As Exception
-
+            Return ex.Message
         Finally
             conexion.Close()
 

5b68a1b6003f05eef6fab633ac9cb5258b4a4144
Oihan Urbeltz <oiihann95@gmail.com>
2026-05-04 13:34:14 +0200

    tareas

--- Frontend/FrmTarea.vb
+++ Frontend/FrmTarea.vb
@@ -1,13 +1,3 @@
 ﻿Public Class FrmTarea
-    Private Sub SplitContainer1_Panel1_Paint(sender As Object, e As PaintEventArgs) Handles SplitContainer1.Panel1.Paint
 
-    End Sub
-
-    Private Sub Label1_Click(sender As Object, e As EventArgs)
-
-    End Sub
-
-    Private Sub Label1_Click_1(sender As Object, e As EventArgs) Handles Label1.Click
-
-    End Sub
 End Class

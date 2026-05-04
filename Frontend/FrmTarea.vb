880c6faebe4042a9ce6a30887c860294c2b313af
Anne Suárez López <anneslop5@gmail.com>
2026-05-04 13:19:06 +0200

    formulario tareas

---
+++ Frontend/FrmTarea.vb
@@ -0,0 +1,13 @@
+﻿Public Class FrmTarea
+    Private Sub SplitContainer1_Panel1_Paint(sender As Object, e As PaintEventArgs) Handles SplitContainer1.Panel1.Paint
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label1_Click_1(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+End Class

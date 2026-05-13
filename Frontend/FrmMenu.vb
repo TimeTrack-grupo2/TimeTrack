0ae6c10b6e5c7ac78ba03c2d5abbb24d3fea71ed
Mario <marioalamolcoz@email.com>
2026-05-13 09:53:09 +0200

    Quitar espacio

--- Frontend/FrmMenu.vb
+++ Frontend/FrmMenu.vb
@@ -3,8 +3,6 @@ Imports Clases
 Imports Gestion
 
 Public Class FrmMenu
-
-
     Private Sub btnDatos_Click(sender As Object, e As EventArgs) Handles btnDatos.Click
         If logIn = GestionAlumno.TipoLogin.Administrador Then
             FrmAlumnos.ShowDialog()

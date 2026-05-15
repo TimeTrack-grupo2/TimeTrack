961440f96af3112c9749f16802416c40c3dbcd33
IzanV <izan.venturav@gmail.com>
2026-05-15 09:47:59 +0200

    Test Acceso con Dni

---
+++ Tests/AccesoDniTest.vb
@@ -0,0 +1,44 @@
+﻿Imports NUnit.Framework
+Imports NUnit.Framework.Constraints
+Imports Gestion
+
+Namespace Tests
+
+    <TestFixture>
+    Public Class Tests
+
+        Private gestionAlumno As GestionAlumno
+
+        <SetUp>
+        Public Sub Setup()
+
+            Dim mensaje As String = ""
+            gestionAlumno = New GestionAlumno(mensaje)
+
+        End Sub
+
+        <Test>
+        Public Sub LogIn_DniCorrecto_DevuelveAlumno()
+
+            Dim mensaje As String = ""
+
+            Dim resultado = gestionAlumno.LogIn("12345678Z", mensaje)
+
+            Assert.That(resultado, NUnit.Framework.Is.EqualTo(GestionAlumno.TipoLogin.Alumno))
+
+        End Sub
+
+        <Test>
+        Public Sub LogIn_DniIncorrecto_DevuelveIncorrecto()
+
+            Dim mensaje As String = ""
+
+            Dim resultado = gestionAlumno.LogIn("AAAA", mensaje)
+
+            Assert.That(resultado, NUnit.Framework.Is.EqualTo(GestionAlumno.TipoLogin.Incorrecto))
+
+        End Sub
+
+    End Class
+
+End Namespace

cb8e0bedd218d2489c103b9f60c3824e9e2751e1
Anne Suárez López <anneslop5@gmail.com>
2026-05-18 11:49:56 +0200

    PU bbdd

--- Tests/PU_ConexionBBDD.vb
+++ Tests/PU_ConexionBBDD.vb
@@ -1,31 +1,42 @@
-﻿<TestClass()>
-Public Class PU_ConexionBBDD
+﻿Imports NUnit.Framework
+Imports Gestion
+Imports System.Data.SqlClient
 
-    Public Class ConexionBD
+Namespace Tests
+    <TestFixture>
+    Public Class ConexionTests
 
-        Private _connectionString As String
+        Private gestionBD As BuscarServidor
 
-        Public Sub New(connectionString As String)
-            _connectionString = connectionString
+        <SetUp>
+        Public Sub Setup()
+            Dim msg As String = ""
+
+            gestionBD = New GestionAlumno(msg)
         End Sub
 
-        <TestMethod()>
-        Public Function PruebaConexion() As Boolean
-            Try
-                Dim conn As New SqlClient.SqlConnection(_connectionString)
-                conn.Open()
-                conn.Close()
-                Return True
-            Catch ex As Exception
-                Return False
-            End Try
-        End Function
+        <Test>
+        Public Sub BaseDatos_DebeEncontrarServidorYConectar()
 
-        <TestMethod()>
-        Public Function ObtenerConnectionString() As String
-            Return _connectionString
-        End Function
+            Dim errorServidor As String = ""
 
+            Dim servidorDetectado As String = gestionBD.BuscarServidor(errorServidor)
+
+            Assert.That(servidorDetectado, [Is].Not.Null.And.Not.Empty,
+                $"No se encontró ningún servidor SQL activo. Último error: {errorServidor}")
+
+            Assert.That(errorServidor, [Is].Empty, $"Hubo un error en el proceso de búsqueda: {errorServidor}")
+        End Sub
+
+        <Test>
+        Public Sub Consulta_CargaDatosDeGrupo2_TrasConectar()
+
+            Dim mensaje As String = ""
+            'login de prueba 
+            Dim resultado = gestionBD.LogIn("12345678Z", mensaje)
+
+            Assert.That(mensaje, [Is].Not.Contains("network-related"),
+                "Error de red al intentar consultar la base de datos GRUPO2.")
+        End Sub
     End Class
-
-End Class
+End Namespace

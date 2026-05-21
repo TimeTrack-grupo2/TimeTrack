11621f424a0bcf4759eff5fef76a935eb01a635a
Anne Suárez López <anneslop5@gmail.com>
2026-05-21 08:52:19 +0200

    Prueba Unitaria BBDD

---
+++ Tests/ConexionBBDDTest.vb
@@ -0,0 +1,19 @@
+﻿Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports System.Data.SqlClient
+
+<TestClass()>
+Public Class BaseDatosTests
+
+    <TestMethod()>
+    Public Sub Test_VerificarConexion_Exitosa()
+        Dim gestorBD As New BuscarServidor()
+
+        Dim cadenaConexionValida As String = "Server=localhost;Database=GRUPO2;Trusted_Connection=True;"
+        Dim estadoEsperado As String = "Open"
+
+        Dim estadoReal As String = gestorBD.IntentarConectar(cadenaConexionValida)
+
+        Assert.AreEqual(estadoEsperado, estadoReal)
+    End Sub
+
+End Class
